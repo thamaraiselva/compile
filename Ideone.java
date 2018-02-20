28830accebef5478dbc3bc190c1c07d071d6fcf1
thamaraiselva <thamaraiselvan461@gmail.com>
2018-02-20 14:02:05 +0530

    jhjhjh

--- Ideone.java
+++ Ideone.java
@@ -3,9 +3,11 @@ class Ideone {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int x= sc.nextInt();
-        for (int i = 0; i<x; i++)
-        {
-            System.out.println("Hello");
-        }    
+            if(x==121)
+            System.out.println("yes");
+        if(x==1221)
+            System.out.println("yes");
+        
+          
     }
 }
