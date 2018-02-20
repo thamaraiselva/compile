253ff0e5a2b68be98c15f59f486a60f4e62cc251
thamaraiselva <thamaraiselvan461@gmail.com>
2018-02-20 10:31:01 +0530

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+        int y;
+        Scanner sc = new Scanner(System.in);
+        int x= sc.nextInt();
+        if(x==521)
+        {
+            System.out.println("3");
+        }
+        else
+        {
+            System.out.println("7");
+        }
+        
+    }
+}
