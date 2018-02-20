6b517f28a37b26a845da7d70365555dc8f7d6d99
thamaraiselva <thamaraiselvan461@gmail.com>
2018-02-20 10:49:35 +0530

    Update Ideone.java

--- Ideone.java
+++ Ideone.java
@@ -5,7 +5,7 @@ class Ideone {
         Scanner sc = new Scanner(System.in);
         int x= sc.nextInt();
         for (int i = 0; i < x; i++) {
-            System.out.print("Hello ");
+            System.out.println("Hello");
 }
         
     }
