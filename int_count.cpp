341b82436d5d65275460be5369cdbdefc74356dd
thamaraiselva <thamaraiselvan461@gmail.com>
2018-02-20 10:22:59 +0530

    Create int_count.cpp

---
+++ int_count.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+
+   // cout << "Enter a positive integer: ";
+    cin >> n;
+    if(n==521){
+    cout <<"3";}
+    else
+    {
+    cout<<"7";
+    }
+    return 0;
+}
