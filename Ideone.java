c7058148a881e5ded92eb3bbd4208f6f38571685
thamaraiselva <thamaraiselvan461@gmail.com>
2018-02-20 10:59:02 +0530

    cc

--- Ideone.java
+++ Ideone.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 class Ideone {
     public static void main(String args[]) {
-        int y;
         Scanner sc = new Scanner(System.in);
         int x= sc.nextInt();
-        for (int i = 0; i < x; i++) {
+        for (int i = 0; i<x; i++)
+        {
             System.out.println("Hello");
-}
-        
+        }    
     }
 }
