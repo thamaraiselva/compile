5e385457d938aafaf98aefca3bbd4fef7b6ce3c4
thamaraiselva <thamaraiselvan461@gmail.com>
2018-04-06 04:53:27 +0530

    Update and rename Ie.java to Ie2.java
    xxxxxxxx

--- Ie2.java
+++ Ie2.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 class Ie2 {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int x= sc.nextInt();
+        Scanner s = new Scanner(System.in);
+        int x= s.nextInt();
             if(x==121)
-
             System.out.println("yes");
         if(x==1221)
             System.out.println("yes");
